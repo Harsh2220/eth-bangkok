c294f123616fee9590bc4ee71b56a53eece19814
Mohit Thakkar <thakkarmohit90@gmail.com>
2024-11-17 03:11:26 +0700

    feat:token details api added

--- src/app/api/tokendetails/route.ts
+++ src/app/api/tokendetails/route.ts
@@ -3,9 +3,10 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req : NextRequest) {
     const { searchParams } = new URL(req.url);
     const addresses = searchParams.get("addresses");
+    const timerange = searchParams.get("timerange");
   try {
     const url = new URL(
-      `https://api.1inch.dev/portfolio/portfolio/v4/overview/erc20/current_value?addresses=${addresses}&use_cache=true`
+      `https://api.1inch.dev/portfolio/portfolio/v4/overview/erc20/details?addresses=${addresses}&use_cache=true&timerange=${timerange}`
     );
 
     const response = await fetch(url, {
