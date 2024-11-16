e7a35100ee77b7a1282797304a7191d738df6b24
Mohit Thakkar <thakkarmohit90@gmail.com>
2024-11-16 22:01:40 +0700

    APIs added

---
+++ src/app/api/tokenpnl/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  //   const { searchParams } = new URL(req.url);
+  //   const addresses = searchParams.get("addresses");
+  console.log("first");
+  try {
+    const url = new URL(
+      "https://api.1inch.dev/portfolio/portfolio/v4/overview/erc20/profit_and_loss?addresses=0x111423FA917A010A4f62c9B2742708744B4CbFc4&use_cache=true"
+    );
+
+
+    const response = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${process.env.INCH_API_KEY}`,
+        Accept: "application/json",
+      },
+    });
+
+    const data = await response.json();
+    return NextResponse.json({ data: data, status: 200 });
+  } catch (error) {
+    return NextResponse.json({ data: error, status: 500 });
+  }
+}
