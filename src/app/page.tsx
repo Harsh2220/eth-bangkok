95ee0d612779465a1c8835e3e8a947eb9bb168f9
Harsh <harshgajjar145@gmail.com>
2024-11-17 01:37:29 +0700

    fix: chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ import { useEffect, useState } from "react";
 import { TrendingUp } from "lucide-react";
 
 const page = () => {
-  const {address} = useAppKitAccount();
+  const { address } = useAppKitAccount();
   const [value, setValue] = useState(0);
   const [percent, setPercent] = useState(0);
   const [pnl, setPnl] = useState(0);
@@ -35,18 +35,18 @@ const page = () => {
       const BASE_URL = process.env.BASE_URL;
       const response = await fetch(`/api/allnetworks?addresses=${address}`);
       const data = await response.json();
-  
+
       // console.log("result", data);
-      
+
       // const responsePercentage = await fetch(
       //   "http://localhost:3000/api/allnetworkspercentage"
       // );
       // const resultPercentage = await responsePercentage.json();
       // console.log("resultPercentage", resultPercentage);
-  
+
       // // Access the data from your response structure
       // const data = result.data.result[0].value_usd;
-  
+
       // Now you can use the data
       // console.log(data.data, "asdsad");
       setValue(data.data.result[0].value_usd.toFixed(5));
@@ -59,26 +59,21 @@ const page = () => {
 
   const fetchPNL = async () => {
     try {
-      const responsePnl = await fetch(
-        `/api/generalpnl?addresses=${address}`
-      );
+      const responsePnl = await fetch(`/api/generalpnl?addresses=${address}`);
       console.log("===> ", responsePnl);
-      
+
       const resultPercentage = await responsePnl.json();
-      
 
       console.log("resultPercentage", resultPercentage);
     } catch (error) {
       console.log("Error fetching data:", error);
       return null;
     }
-  }
+  };
 
   useEffect(() => {
     if (address) {
       fetchAmount();
-
-      
     }
   }, [address]);
 
@@ -110,16 +105,16 @@ const page = () => {
         </div>
       </div>
       <div className="flex gap-4">
-        <div className="w-[content] ">
+        <div className="w-4/12">
           <PieChartComponent />
         </div>
-        <div className=" border-[0.5px] border-gray-800 ml-2 mr-2" />
-        <div className="">
+        <div className="border-[0.5px] border-gray-800 ml-2 mr-2" />
+        <div className="w-8/12">
           <LineChartComponent />
         </div>
       </div>
       <div>
-        <Card className="flex flex-col bg-transparent  w-[content]">
+        <Card className="flex flex-col bg-transparent w-[content]">
           <CardHeader className="items-center pb-0">
             <CardTitle className=" w-full">Tokens</CardTitle>
             {/* <CardDescription>Overview</CardDescription> */}
