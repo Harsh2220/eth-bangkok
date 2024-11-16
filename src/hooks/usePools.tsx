1130a35a12eeee0c95970d3fe3f5171fe2fc18db
ArpitaGanatra <ganatraarpita@gmail.com>
2024-11-17 00:41:59 +0700

    fix: build

--- src/hooks/usePools.tsx
+++ src/hooks/usePools.tsx
@@ -1,15 +1,14 @@
-import { ABI } from "@/constants/abi";
-import getPublicClient from "@/utils/getPublicClient";
+// import { ABI } from "@/constants/abi";
+// import getPublicClient from "@/utils/getPublicClient";
 import getTokenContract from "@/utils/getTokenContract";
-import { arbitrum, base, optimism, polygon } from "viem/chains";
 import { SUPPORTED_CHAINS, SUPPORTED_TOKENS } from "@/constants";
-import { POLYGON_CONTRACT } from "@/constants/contracts";
+// import { POLYGON_CONTRACT } from "@/constants/contracts";
 
 export default function usePools() {
   async function getPools() {
     for (let index = 0; index < SUPPORTED_TOKENS.length; index++) {
       for (let index = 0; index < SUPPORTED_CHAINS.length; index++) {
-        const publicClient = getPublicClient(SUPPORTED_CHAINS[index]);
+        // const publicClient = getPublicClient(SUPPORTED_CHAINS[index]);
 
         const tokenAddress = getTokenContract(
           SUPPORTED_CHAINS[index],
@@ -20,12 +19,12 @@ export default function usePools() {
           return index++;
         }
 
-        const data = await publicClient.readContract({
-          abi: ABI,
-          address: POLYGON_CONTRACT,
-          functionName: "getReserveData",
-          args: [tokenAddress],
-        });
+        // const data = await publicClient.readContract({
+        //   abi: ABI,
+        //   address: POLYGON_CONTRACT,
+        //   functionName: "getReserveData",
+        //   args: [tokenAddress],
+        // });
       }
     }
   }
