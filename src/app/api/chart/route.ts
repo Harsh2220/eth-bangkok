88838f658b8afc333fe14a510e021c9afc0feedf
Mohit Thakkar <thakkarmohit90@gmail.com>
2024-11-17 08:58:11 +0700

    fix: chart api

--- src/app/api/chart/route.ts
+++ src/app/api/chart/route.ts
@@ -1,12 +1,23 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
-export async function GET() {
-  //   const { searchParams } = new URL(req.url);
-  //   const addresses = searchParams.get("addresses");
+export async function GET(req:NextRequest ) {
+    const { searchParams } = new URL(req.url);
+    const addresses = searchParams.get("addresses");
+    const timerange = searchParams.get("timerange");
+    const chain_id = searchParams.get("chain_id");
   try {
     const url = new URL(
-      "https://api.1inch.dev/portfolio/portfolio/v4/general/value_chart?addresses=0x111423FA917A010A4f62c9B2742708744B4CbFc4&timerange=1week"
+      `https://api.1inch.dev/portfolio/portfolio/v4/general/value_chart?addresses=${addresses}`
     );
+
+    if (chain_id) {
+      url.searchParams.append("chain_id", chain_id);
+    }
+    if (timerange) {
+      url.searchParams.append("timerange", timerange);
+    } else {
+      url.searchParams.append("timerange", "1year");
+    }
 
     const response = await fetch(url, {
       headers: {
